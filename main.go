c36722df6d8d38c124f0c1bcec9bb646fe318ae7
AKSHAYK0UL <akshaykoul999@gmail.com>
2024-07-07 22:15:40 +0530

    minor bug fixes

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 	"github.com/AKSHAYK0UL/Email_Auth/routefunc"
 )
 
-func init() {
-	model.ConnectMongo()
-}
+//	func init() {
+//		model.ConnectMongo()
+//	}
 func main() {
 	fmt.Println("NEW Auth Server")
+	model.ConnectMongo()
 	route := routefunc.RouteTable()
 	port := os.Getenv("PORT")
 	if port == "" {
